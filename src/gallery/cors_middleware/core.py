6b0431c17f1366be645d286dabf1d0b3d00dd127
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-06-28 11:02:38 +0400

    CORS

---
+++ src/gallery/cors_middleware/core.py
@@ -0,0 +1,21 @@
+from django import http
+
+# https://stackoverflow.com/questions/28046422/django-cors-headers-not-work
+
+# Why use this instead of django-cors-middleware?
+# Module above is not working. So I end with that solution.
+
+class CorsMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if (request.method == "OPTIONS"  and "HTTP_ACCESS_CONTROL_REQUEST_METHOD" in request.META):
+            response = http.HttpResponse()
+            response["Content-Length"] = "0"
+            response["Access-Control-Max-Age"] = 86400
+        response["Access-Control-Allow-Origin"] = "*"
+        response["Access-Control-Allow-Methods"] = "DELETE, GET, OPTIONS, PATCH, POST, PUT"
+        response["Access-Control-Allow-Headers"] = "accept, accept-encoding, authorization, content-type, dnt, origin, user-agent, x-csrftoken, x-requested-with"
+        return response
--- src/gallery/gallery/settings.py
+++ src/gallery/gallery/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'cors_middleware.core.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
